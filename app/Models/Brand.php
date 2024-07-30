e307cd8d23ecce6b6c3777bba310208fb637a9c8
haweil <92003311+haweil@users.noreply.github.com>
2024-07-30 12:40:41 +0300

    Set up Filament 3 and make Brand&Category Resource

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -12,7 +12,7 @@ class Brand extends Model
     protected $fillable = [
         'name',
         'slug',
-        'image',
+        'logo',
         'is_active'
     ];
     public function products()
