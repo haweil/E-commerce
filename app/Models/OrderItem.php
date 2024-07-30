85fe4f17dbcf2a071cd528c6011e8741654beddc
haweil <92003311+haweil@users.noreply.github.com>
2024-07-30 10:58:40 +0300

    Make Models And migration

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Order;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class OrderItem extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'product_id',
+        'order_id',
+        'quantity',
+        'unit_amount',
+        'total_amount',
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
