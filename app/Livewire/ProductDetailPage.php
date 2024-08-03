0cdc1c36e31929c0ec78b7e84a2fdf1a4ffaf888
haweil <92003311+haweil@users.noreply.github.com>
2024-08-03 08:26:23 +0300

    Make Product & ProductDetails

--- app/Livewire/ProductDetailPage.php
+++ app/Livewire/ProductDetailPage.php
@@ -2,12 +2,23 @@
 
 namespace App\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
+
+#[Title('Product Detail - eShopXpert')]
 class ProductDetailPage extends Component
 {
+
+    public $slug;
+    public function mount($slug)
+    {
+        $this->slug = $slug;
+    }
     public function render()
     {
-        return view('livewire.product-detail-page');
+        $product = Product::where('slug', $this->slug)->firstOrFail();
+        return view('livewire.product-detail-page', compact('product'));
     }
 }
--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -2,12 +2,64 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
 use Livewire\Component;
+use App\Models\Category;
+use App\Models\Product;
+use Livewire\Attributes\Title;
+use Livewire\Attributes\Url;
+use Livewire\WithPagination;
 
+#[Title('Products - eShopXpert')]
 class ProductsPage extends Component
 {
+
+    use WithPagination;
+
+    #[Url]
+    public $selected_Categories = [];
+
+    #[Url]
+    public $selected_Brands = [];
+
+    #[Url]
+    public $featured;
+
+    #[Url]
+    public $onSale;
+
+    #[Url]
+    public $price_range = 100;
     public function render()
     {
-        return view('livewire.products-page');
+        $productQuery = Product::query()->where('is_active', 1);
+
+
+        if (!empty($this->selected_Categories)) {
+            $productQuery->whereIn('category_id', $this->selected_Categories);
+        }
+
+        if (!empty($this->selected_Brands)) {
+            $productQuery->whereIn('brand_id', $this->selected_Brands);
+        }
+
+        if ($this->featured) {
+            $productQuery->where('is_featured', 1);
+        }
+
+        if ($this->onSale) {
+            $productQuery->where('on_sale', 1);
+        }
+
+        if ($this->price_range) {
+            $productQuery->whereBetween('price', [0, $this->price_range]);
+        }
+        $categories = Category::where('is_active', true)->get(['id', 'name', 'slug']);
+        $brands = Brand::where('is_active', true)->get(['id', 'name', 'slug']);
+        return view('livewire.products-page', [
+            'categories' => $categories,
+            'brands' => $brands,
+            'products' => $productQuery->paginate(9),
+        ]);
     }
 }
