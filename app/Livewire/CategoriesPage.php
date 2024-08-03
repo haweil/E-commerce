c71860ca92a69befaa5f41907ac364587ed693a3
haweil <92003311+haweil@users.noreply.github.com>
2024-08-03 06:38:18 +0300

    Make Categories Dynamic

--- app/Livewire/CategoriesPage.php
+++ app/Livewire/CategoriesPage.php
@@ -3,11 +3,15 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Category;
+use Livewire\Attributes\Title;
 
+#[Title('Categories', 'eShopXpert')]
 class CategoriesPage extends Component
 {
     public function render()
     {
-        return view('livewire.categories-page');
+        $categories = Category::where('is_active', true)->get();
+        return view('livewire.categories-page', compact('categories'));
     }
 }
--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -2,12 +2,18 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
 use Livewire\Component;
+use App\Models\Category;
+use Livewire\Attributes\Title;
 
+#[Title('Home Page - eShopXpert')]
 class HomePage extends Component
 {
     public function render()
     {
-        return view('livewire.home-page');
+        $brands = Brand::where('is_active', true)->get();
+        $categories = Category::where('is_active', true)->get();
+        return view('livewire.home-page', compact('categories', 'brands'));
     }
 }
